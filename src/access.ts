bbcb949b835b736875ff1454c1805036668365cf
linzeguang <15817289730@163.com>
2022-11-18 16:28:44 +0800

    first commit

---
+++ src/access.ts
@@ -0,0 +1,7 @@
+export default (initialState: any) => {
+  // 在这里按照初始化数据定义项目中的权限，统一管理
+  // 参考文档 https://next.umijs.org/docs/max/access
+  console.log('initialState',initialState)
+  return {
+  };
+};
